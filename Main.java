d95d76a5742de51e50f7763b4c2153df730aa642
datboisus <donvu251106@gmail.com>
2024-10-07 10:13:55 -0700

    1

--- Main.java
+++ Main.java
@@ -4,6 +4,15 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Circle c1 = new Circle(10.1);
+    Circle c2 = new Circle(14.0);
+    Circle c3 = new Circle(20.5);
+    String circle1 = c1.toString();
+    String circle2 = c2.toString();
+    String circle3 = c3.toString();
+    System.out.println(circle1 + "\n" + circle2 + "\n" + circle3);
+
+    Scanner sc = new Scanner(System.in);
+    
   }
 }
